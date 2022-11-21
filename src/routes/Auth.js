8c25cdb1354ec8d263f3d3ed0a436da6af5014f4
seodoha <doha.seo89@gmail.com>
2022-11-21 17:01:31 +0900

    33

---
+++ src/routes/Auth.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { authService } from '../fbase';
+
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const onChange = (event) => {
+        const {target: {name, value}} = event;
+        if(name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+    const onSubmit = async(event) => {
+        event.preventDefault();
+        if(newAccount) {
+            // create account
+            await authService.createUserWithEmailAndPassword (
+                email, password
+            )
+        } else {
+            // log in
+            await authService.signInWithEmailAndPassword (
+                email, password
+            )
+        }
+
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    name="email"
+                    type="email"
+                    placeholder='Email'
+                    required
+                    value={email}
+                    onChange={onChange}
+                />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder='Password'
+                    required
+                    value={password}
+                    onChange={onChange}
+                />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    );
+};
+
+export default Auth;
